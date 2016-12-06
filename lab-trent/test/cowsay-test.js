1d49d3bd79f21bc5da8486ace59e524991865f6e
Trenton Kress <trenton.kress@gmail.com>
2016-12-05 22:41:43 -0800

    Wrote a test kind of lazily since I cba to get the actual text and format it with \n and what not.

--- lab-trent/test/cowsay-test.js
+++ lab-trent/test/cowsay-test.js
@@ -1,17 +1,13 @@
 'use strict';
 
 const cowsayRes = require('../lib/cowsayres');
+const cowsay = require('cowsay');
 const assert = require('assert');
 
 describe('CowsayResponse', function() {
-  describe('#getCowsay()', function() {
+  describe('#cowsayGet()', function() {
     it('should return cowsay based on the body/text given', function() {
-
-    });
-  });
-  describe('#postCowsay()', function() {
-    it('should return cowsay based on the body/text given', function() {
-
+      assert.equal(cowsayRes.cowsayGet({ text: 'whoah' }), cowsay.say({ text: 'whoah' }));
     });
   });
 });
