2cc9056c89fa76e981c100292abdd3618ed03fc4
Trenton Kress <trenton.kress@gmail.com>
2016-12-05 21:29:38 -0800

    Scaffolded the project and laid out a foundation for each file.

---
+++ lab-trent/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res) {
+  
+});
+
+server.listen(PORT);
---
+++ lab-trent/test/cowsay-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const cowsayhttp = require('../lib/cowsayhttp');
+const assert = require('assert');
+
+describe('CowsayHTTP', function() {
+  describe('#getCowsay()', function() {
+    it('should return cowsay based on the text given', function() {
+
+    });
+  });
+});
