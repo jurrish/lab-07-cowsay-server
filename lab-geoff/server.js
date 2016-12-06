d5315b6490b7e39ebaa919080a0376aa11393a00
Geoffrey Cox <gcox3d@gmail.com>
2016-12-05 18:39:31 -0800

    added querystring to server

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -2,11 +2,14 @@
 
 let http = require('http');
 let url = require('url');
+let querystring = require('querystring');
 
 let PORT = 3000;
 
 let server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+  console.log(req.url.query);
   console.log(req.url);
   res.write('hello world');
   res.end();
