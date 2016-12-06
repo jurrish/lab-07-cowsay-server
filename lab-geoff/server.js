d5cd697310e6d2ce4acc122c9652ee7e1dee7646
Geoffrey Cox <gcox3d@gmail.com>
2016-12-05 21:02:16 -0800

    cow will now say querystring.

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -13,28 +13,30 @@ let server = http.createServer(function(req, res) {
   req.url.query = querystring.parse(req.url.query);
   console.log(req.url.query);
 
-  res.writeHead(200, {
-    'Content-type' : 'text/plain',
-  });
-  console.log(cowsay.say({
-    text: 'i am cow',
-  }));
-  res.write('hello world \n');
-  res.end();
+  // res.writeHead(200, {
+  //   'Content-type' : 'text/plain',
+  // });
+  // console.log(cowsay.say({
+  //   text: 'i am cow',
+  // }));
+  // res.write('hello world \n');
+  // res.end();
 
   if(req.method === 'POST') {
     console.log('post request block');
     //parse body stuff from handler
   }
 
-  if(req.method === 'GET' && req.url.pathname === '/cowsay') { //add and for /cowsay
+  if(req.method === 'GET' && req.url.pathname === '/cowsay') {
     console.log('cowsay get request block');
     if(req.url.query.text) {
       console.log('text equals message');
       res.writeHead(200, {
         'Content-type': 'text/plain',
       });
-      //cowsay querystring body
+      res.write(cowsay.say({
+        text: `${req.url.query.text}`,
+      }));
       res.end();
     } else {
       console.log('text does not equal message');
