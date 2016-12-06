c2e3aae88d8bec4cb3ec07b87bb91867a5adc4b4
Michael Bishop <polishthebishop@gmail.com>
2016-12-05 20:37:33 -0800

    finish first query for http server

--- lab-michael/server.js
+++ lab-michael/server.js
@@ -6,10 +6,22 @@ const url = require('url');
 const queryString = require('querystring');
 
 const parseBody = require('./lib/bodyParser.js');
+
 const cowsay = require('cowsay');  // Moooooo
 
-module.exports = exports = {};
 
 const PORT = process.env.PORT || 3000;  //usr def or 3000
 
+module.exports = exports = {};
 // Create the server request
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
+
+  if(req.url.pathname === '/') {
+    res.writeHead(200,
+      {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+  }
+});
