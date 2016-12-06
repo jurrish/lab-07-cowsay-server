db49d1851c3a275a3dd30185b97eee7b2817d8b7
Geoffrey Cox <gcox3d@gmail.com>
2016-12-05 18:26:18 -0800

    update server file with callback argument in createServer.

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -1,9 +1,14 @@
 'use strict';
 
 let http = require('http');
-
-let server = http.createServer();
 let PORT = 3000;
+
+let server = http.createServer(function(req, res) {
+  console.log(req);
+  res.write('hello world');
+  res.end();
+  return;
+});
 
 server.listen(PORT, function() {
   console.log('server running on port ' + PORT);
