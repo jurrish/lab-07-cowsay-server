380745e2dddfd8b14c86ca1adce1b0216b28f5cc
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-05 20:09:35 -0800

    GET works. The cow speaks!

--- lab-jessica/lib/server.js
+++ lab-jessica/lib/server.js
@@ -1,54 +1,45 @@
 // Create a HTTP Server using the http module
 const http = require('http');
-const querystring = require('querystring');
+const url = require('url');
+const queryString = require('querystring');
 const cowsay = require('cowsay');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(function(req, res) {
-  console.log(req.url.substring(0,1));
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
+  console.log('req.url.query', req.url.query.text);
+  console.log('req.url', req.url);
+  console.log('req.method', req.method);
+  console.log('req.headers', req.headers);
 
-  if(req.url.substring(0,1) === '/') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world');
-    res.end();
+  if (req.method === 'GET' && req.url.pathname === '/cowsay'){
+    const message = req.url.query.text;
+    if(message) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(cowsay.say({text: message}) + '\n');
+    }
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write(cowsay.say({text:'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
   }
+  // POST REQUEST
+  //
+  // the response header should include Content-Type: text/plain
+  // if the json {text: messsage} is set in the body, respond with:
+  // a status code of 200
+  // a body including the value returned from cowsay.say({text: <querystring text>})
+  // if the json{text: messsage}is not set in the body, respond with:
+  // status code = 400
+  // a body including the value returned from cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'})
 
-  if(req.url.substring(0,7) === '/cowsay') {
-    let message = querystring.parse(req.url);
-    console.log(message);
-    res.write(cowsay.say({text : 'I\'m a moooodule'}) + '\n');
-    res.end();
-  }
+  // if (req.method === 'POST'){
+  //   parseBody(req, function(err){
+  //     if (err) return console.error(err);
+  //   });
+  // }
 
+  res.end();
 });
-
-
-
-// /cowsay
-//
-// GET REQUEST
-//
-// the query string should have the key value text=<message>
-
-// the response header should include Content-Type: text/plain
-// if the query text=messsage is set, respond with:
-// a status code of 200
-// a body including the value returned from cowsay.say({text: <querystring text>})
-// if the query text=message is not set, respond with:
-// status code = 400
-// a body including the value returned from cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'})
-
-
-// POST REQUEST
-//
-// the response header should include Content-Type: text/plain
-// if the json {text: messsage} is set in the body, respond with:
-// a status code of 200
-// a body including the value returned from cowsay.say({text: <querystring text>})
-// if the json{text: messsage}is not set in the body, respond with:
-// status code = 400
-// a body including the value returned from cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'})
-
 
 server.listen(PORT, function () {
   console.log('server running on port', PORT);
