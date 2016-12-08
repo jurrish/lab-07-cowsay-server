658ce0d5844ea484cb3da0103fc081c6be6a0e0a
Dan Peters <daniel.r.peters@icloud.com>
2016-12-07 23:21:55 -0800

    finished tests, added gulp stuff, and finished the readme

--- lab-dan/test/server-test.js
+++ lab-dan/test/server-test.js
@@ -4,26 +4,61 @@ const expect = require('chai').expect;
 
 chai.use(chaiHttp);
 
-const server = require('../server');
-let myServer = undefined;
+const app = require('../index');
 
 describe('this is a basic test of my server', function () {
-  before(function () {
-    myServer = server.start();
-  });
   it('should be up and running after the start function is called', function () {
-    expect(myServer.listening).to.be.true;
+    expect(app.listening).to.be.true;
   });
-  it('should get a Hello World response', function() {
-    chai.request(myServer)
+  it('should get a 200 response for \'/\'', function(done) {
+    chai.request(app)
       .get('/')
       .end(function(err, res) {
-        expect(res).to.have.status(200); 
+        expect(res).to.have.status(200);
+        expect(res).to.be.text;
         done();
       });
   });
+  it('should get a 200 response for a GET request to \'/cowsay?text=test\'', function(done) {
+    chai.request(app)
+    .get('/cowsay?text=test')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.text;
+      done();
+    });
+  });
+  it('should get a 200 response for a POST \'/cowsay\'', function(done) {
+    chai.request(app)
+    .post('/cowsay')
+    .send({ text: 'test'})
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.text;
+      done();
+    });
+  });
+  it('should get a 400 response for \'/cowsay\'', function(done) {
+    chai.request(app)
+    .get('/cowsay')
+    .end(function(err, res) {
+      expect(res).to.have.status(400);
+      expect(res).to.be.text;
+      done();
+    });
+  });
+  it('should get a 400 response for an incorrect POST \'/cowsay\'', function(done) {
+    chai.request(app)
+    .post('/cowsay')
+    .send({ test: 'error'})
+    .end(function(err, res) {
+      expect(res).to.have.status(400);
+      expect(res).to.be.text;
+      done();
+    });
+  });
   after(function (done) {
-    myServer.close(function () {
+    app.close(function () {
       done();
     });
   });
